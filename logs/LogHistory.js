44821c688566517edea2b69437e548f9d8b2b0a9
viachaslau.kanapliou <viachaslau_kanapliou@epam.com>
2015-09-29 00:35:17 +0300

    styling

--- logs/LogHistory.js
+++ logs/LogHistory.js
@@ -1,5 +1,19 @@
 angular.module('LogViewer.LogHistory', [])
     .service('logHistoryService', ['$rootScope',
         function ($rootScope) {
+            $rootScope.loggerIDStorage = [];
+            var service = {
+                addToHistory: addToHistory,
+                clearHistory: clearHistory
+            };
+            function addToHistory(item){
+                if (item && $rootScope.loggerIDStorage.indexOf(item) < 0){
+                    $rootScope.loggerIDStorage.push(item);
+                }
+            }
+            function clearHistory(){
+                $rootScope.loggerIDStorage = [];
+            }
 
+            return service
         }]);
--- menu/navigation/Navigation.js
+++ menu/navigation/Navigation.js
@@ -4,8 +4,8 @@ angular.module('LogViewer.Menu.Navigation', [])
  * Listen 'logsNotLoaded' do 'clean'
  * throw 'headerSizeChanged' ev = {}
  */
-    .controller('NavigationCtrl', ['$scope', 'logService', '$rootScope',
-        function ($scope, logService, $rootScope) {
+    .controller('NavigationCtrl', ['$scope', 'logService', '$rootScope', 'logHistoryService',
+        function ($scope, logService, $rootScope, logHistoryService) {
             $scope.osaCalls = [];
             $scope.activeOsaCall = null;
             $scope.loggerID = null;
@@ -13,8 +13,15 @@ angular.module('LogViewer.Menu.Navigation', [])
 
             $scope.loggerID = '15092710034991c6c3a060a91646e689fc83d4938ff7bd';
 
-            $scope.getLogs = function () {
+            $scope.clear = function(){
+                logHistoryService.clearHistory();
+            };
+            $scope.getLogs = function (loggerID) {
+                if (loggerID) {
+                    $scope.loggerID = loggerID;
+                }
                 logService.getLogs($scope.loggerID);
+                logHistoryService.addToHistory($scope.loggerID);
                 $rootScope.$broadcast('headerSizeChanged');
             };
             $scope.setActiveOsaCall = function() {
